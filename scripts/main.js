9f27213b60a1ab17ce0889229000338266fa804d
NSVEGUR <nsvegur@gmail.com>
2022-02-17 14:25:12 +0530

    created dock - 2

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,32 @@
 import './../styles/style.css';
 
-// document.querySelector('#app').innerHTML = `
-//   <h1>Hello Vite!</h1>
-//   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-// `
+const dockContainer = `<div class="dock-container">
+<button class="icon">
+	<div>
+		<span>DBMS</span>
+		<img class="app-icon" src="assets/icons/dbms.png" />
+	</div>
+	<div>
+		<span>Harsha</span>
+		<img class="app-icon" src="assets/icons/harsha.png" />
+	</div>
+	<div>
+		<span>Praveen</span>
+		<img class="app-icon" src="assets/icons/praveen.png" />
+	</div>
+	<div>
+		<span>Ravindar</span>
+		<img class="app-icon" src="assets/icons/ravindar.png" />
+	</div>
+	<div>
+		<span>Vineeth</span>
+		<img class="app-icon" src="assets/icons/vineeth.png" />
+	</div>
+	<div>
+		<span>Nagasai</span>
+		<img class="app-icon" src="assets/icons/nagasai.png" />
+	</div>
+</button>
+</div>`;
+
+document.querySelector('#dock').innerHTML = dockContainer;
