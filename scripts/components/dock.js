b7338e1091ddba20bcb3ba16aec4080c4c0f6422
Harsha662002 <hvasireddy329@gmail.com>
2022-02-25 23:28:23 +0530

    added information into windows

--- scripts/components/dock.js
+++ scripts/components/dock.js
@@ -1,38 +1,38 @@
-import { dockWindows } from './window.js';
+import { dockWindows } from "./window.js";
 
-const dockIcons = document.querySelectorAll('.icon > div');
-// const mount = [
-// 	document.getElementById('database'),
-// 	document.getElementById('database'),
-// 	document.getElementById('database'),
-// 	document.getElementById('database'),
-// 	document.getElementById('database'),
-// 	document.getElementById('database'),
-// ]
+const dockIcons = document.querySelectorAll(".icon > div");
+const mounts = [
+  document.getElementById("database"),
+  document.getElementById("sql"),
+  document.getElementById("Harsha"),
+  document.getElementById("Praveen"),
+  document.getElementById("Ravindar"),
+  document.getElementById("Vineeth"),
+  document.getElementById("Nagasai"),
+];
 
 export class Dock {
-	constructor() {
-		dockIcons.forEach((icon, i) => {
-			icon.addEventListener('click', (e) => {
-				const index = e.target.dataset.icon;
-				if (index == 8) return window.open("https://github.com/NSVEGUR/dbOS-web", '_blank');
-				if (!dockWindows[index].isOpen) {
-
-					if (index == 7) {
-						dockWindows[index].open({
-							url: 'https://vscode.dev/'
-						});
-					}
-					else {
-						dockWindows[index].open({
-
-						});
-					}
-					icon.classList.add('active');
-				} else {
-					dockWindows[index].focus();
-				}
-			});
-		});
-	}
+  constructor() {
+    dockIcons.forEach((icon, i) => {
+      icon.addEventListener("click", (e) => {
+        const index = e.target.dataset.icon;
+        if (index == 8)
+          return window.open("https://github.com/NSVEGUR/dbOS-web", "_blank");
+        if (!dockWindows[index].isOpen) {
+          if (index == 7) {
+            dockWindows[index].open({
+              url: "https://vscode.dev/",
+            });
+          } else {
+            dockWindows[index].open({
+              mount: mounts[index].cloneNode(true),
+            });
+          }
+          icon.classList.add("active");
+        } else {
+          dockWindows[index].focus();
+        }
+      });
+    });
+  }
 }
